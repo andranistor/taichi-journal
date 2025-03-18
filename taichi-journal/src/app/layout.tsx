a3efbc7796c45bdb4f6958427bc33eb9aaacc4f8
Andra Nistor <andra.nistor@seznam.cz>
2025-03-18 15:53:53 +0100

    Initial commit. Next.js and Material UI installation.

---
+++ taichi-journal/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { Roboto } from "next/font/google";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.variable}>
+        <ThemeProvider theme={theme}>
+          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
